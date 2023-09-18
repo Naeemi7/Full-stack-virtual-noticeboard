752aa71b36ac2a78e14d4bc9ac8e3dc69bcd50fd
Abdulwase Naeemi <abdulwasenaeemi7@gmail.com>
2023-09-18 14:45:14 +0200

    bug fixed

--- server.js
+++ server.js
@@ -3,7 +3,14 @@ import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
 import noticeRoute from "./routes/noticeRoute.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 dotenv.config();
 
@@ -33,6 +40,12 @@ mongoose
   });
 
 app.use("/notices", noticeRoute);
+
+// Serve frontend client/build folder
+app.use(express.static(path.join(__dirname, "client/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/dist/index.html"));
+});
 
 app.listen(port, () => {
   console.log(`The server 🙈 is listening on port ${port}`);
