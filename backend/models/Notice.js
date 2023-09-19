3b0d48dbb1e0a068812b0cee85e961208dc85963
Abdulwase Naeemi <abdulwasenaeemi7@gmail.com>
2023-09-19 14:34:46 +0200

    bug fixed

--- backend/models/Notice.js
+++ backend/models/Notice.js
@@ -3,7 +3,13 @@ import { Schema, model } from "mongoose";
 const noticeSchema = new Schema({
   text: { type: String, required: true },
   author: { type: String, required: true },
-  date: { type: Date, default: Date.now },
+  date: {
+    type: Date,
+    default: Date.now,
+    get: function () {
+      return this._doc.date.toLocaleDateString();
+    },
+  },
 });
 
 const Notice = model("Notice", noticeSchema);
